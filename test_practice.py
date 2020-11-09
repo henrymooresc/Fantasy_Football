9e5355be697cafa0d8bb1dda557c662762ed2dd9
henrymooresc <henrypmoore123@gmail.com>
2020-11-09 18:05:11 -0500

    part 7

--- test_practice.py
+++ test_practice.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def covariance(x, y):
+    n = len(x)
+    return sum((x - np.mean(x)) * (y - np.mean(y))) * 1/(n-1)
+
+def correlation(x, y):
+    return covariance(x, y)/(np.std(x) * np.std(y))
 
 csv_path = '2019.csv'
 
@@ -10,4 +20,9 @@ df['Usage'] = df['Tgt'] + df['RushingAtt']
 df['UsageRank'] = df['Usage'].rank(ascending=False)
 df['FantasyPointsRank'] = df['FantasyPoints'].rank(ascending=False)
 
-print(df.sort_values(by='UsageRank').head(15))
+df.sort_values(by='UsageRank').head(15)
+
+x = df['Usage'].values
+y = df['FantasyPoints'].values
+
+correlation(x, y)
