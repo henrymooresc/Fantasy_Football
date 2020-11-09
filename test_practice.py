e50cc2317347708e4e9759f7ce3799c520e85ccf
henrymooresc <henrypmoore123@gmail.com>
2020-11-09 17:47:09 -0500

    part 5

--- test_practice.py
+++ test_practice.py
@@ -4,4 +4,10 @@ csv_path = '2019.csv'
 
 df = pd.read_csv(csv_path)
 
-df.head()
+df = df.loc[df['Pos'] == 'RB', ['Player', 'Tgt', 'RushingAtt', 'FantasyPoints']]
+df['Usage'] = df['Tgt'] + df['RushingAtt']
+
+df['UsageRank'] = df['Usage'].rank(ascending=False)
+df['FantasyPointsRank'] = df['FantasyPoints'].rank(ascending=False)
+
+print(df.sort_values(by='UsageRank').head(15))
