36408e2db3989376f10a4f4eeeec1c40d49f15c9
henrymooresc <henrypmoore123@gmail.com>
2020-11-09 20:50:48 -0500

    part 1 intermediate

--- test_practice.py
+++ test_practice.py
@@ -1,27 +1,81 @@
 import pandas as pd
-import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
 
-def covariance(x, y):
-    n = len(x)
-    return sum((x - np.mean(x)) * (y - np.mean(y))) * 1/(n-1)
+df = pd.read_csv('2019.csv')
 
-def correlation(x, y):
-    return covariance(x, y)/(np.std(x) * np.std(y))
+df.drop(['Rk', '2PM', '2PP', 'FantPt', 'DKPt', 'FDPt', 'VBD', 'PosRank', 'OvRank', 'PPR', 'Fmb', 'GS'], axis=1, inplace=True)
 
-csv_path = '2019.csv'
+df['Player'] = df['Player'].apply(lambda x: x.split('*')[0]).apply(lambda x: x.split('\\')[0])
 
-df = pd.read_csv(csv_path)
+df.rename({
+    'TD': 'PassingTD',
+    'TD.1': 'RushingTD',
+    'TD.2': 'ReceivingTD',
+    'TD.3': 'TotalTD',
+    'Yds': 'PassingYDs',
+    'Yds.1': 'RushingYDs',
+    'Yds.2': 'ReceivingYDs',
+    'Att': 'PassingAtt',
+    'Att.1': 'RushingAtt'
+}, axis=1, inplace=True)
 
-df = df.loc[df['Pos'] == 'RB', ['Player', 'Tgt', 'RushingAtt', 'FantasyPoints']]
-df['Usage'] = df['Tgt'] + df['RushingAtt']
+rb_df = df[df['FantPos'] == 'RB']
+qb_df = df[df['FantPos'] == 'QB']
+wr_df = df[df['FantPos'] == 'WR']
+te_df = df[df['FantPos'] == 'TE']
 
-df['UsageRank'] = df['Usage'].rank(ascending=False)
-df['FantasyPointsRank'] = df['FantasyPoints'].rank(ascending=False)
+rushing_columns = ['RushingAtt', 'RushingYDs', 'Y/A', 'RushingTD',]
+receiving_columns = ['Tgt', 'Rec', 'ReceivingYDs', 'Y/R', 'ReceivingTD']
+passing_columns = ['PassingAtt', 'PassingYDs', 'PassingTD', 'Int']
 
-df.sort_values(by='UsageRank').head()
+def transform_columns(df, new_column_list):
+    df = df[['Player', 'Tm', 'Age', 'G'] + new_column_list + ['FL']]
+    return df
 
-X = df['Usage'].values
-Y = df['FantasyPoints'].values
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+dumbthing = rushing_columns + receiving_columns
+rb_df = transform_columns(rb_df, dumbthing)
+#wr_df = transform_columns(wr_df, dumbthing)
+#te_df = transform_columns(te_df, receiving_columns)
+#qb_df = transform_columns(qb_df, passing_columns)
+
+
+rb_df['FantasyPoints'] = rb_df['RushingYDs']*0.1 + rb_df['RushingTD']*6 + rb_df['Rec'] + rb_df['ReceivingYDs']*0.1 + rb_df ['ReceivingTD']*6 - rb_df['FL']*2
+
+rb_df['FantasyPoints/GM'] = rb_df['FantasyPoints']/rb_df['G']
+rb_df['FantasyPoints/GM'] = rb_df['FantasyPoints/GM'].apply(lambda x: round(x, 2))
+rb_df['Usage/GM'] = (rb_df['RushingAtt'] + rb_df['Tgt'])/rb_df['G']
+rb_df['Usage/GM'] = rb_df['Usage/GM'].apply(lambda x: round(x, 2))
+
+
+'''
+#just for styling. We imported seaborn earlier as sns.
+sns.set_style('whitegrid')
+
+#create a canvas with matplotlib
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 10)
+
+#basic regression scatter plot with trendline
+plot = sns.regplot(
+x=rb_df['Usage/GM'],
+y=rb_df['FantasyPoints/GM'],
+scatter=True,)
+
+plt.show()
+'''
+
+rb_df['TD/Usage'] = (rb_df['RushingTD'] + rb_df['ReceivingTD']) / (rb_df['RushingAtt'] + rb_df['Tgt'])
+
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 10)
+
+rb_df = rb_df[rb_df['RushingAtt'] > 20]
+
+plot = sns.regplot(
+    x=rb_df['TD/Usage'],
+    y=rb_df['FantasyPoints/GM'],
+    scatter=True
+)
+
+plt.show()
