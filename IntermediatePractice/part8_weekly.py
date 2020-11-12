4af6825fb607c1db622c4f5f475724ecef00d5d6
Henry Moore Work <henrypmoore123@gmail.com>
2020-11-12 14:37:18 -0500

    part 8 start

---
+++ IntermediatePractice/part8_weekly.py
@@ -0,0 +1,6 @@
+import pandas as pd # Python's data munging library
+from bs4 import BeautifulSoup as BS # Python's web scraping library
+import requests # Python's library for making HTTP requests
+from matplotlib import pyplot as plt # for visualizing the data
+import seaborn as sns # for styling the visualizations
+import numpy as np # for doing some maths
